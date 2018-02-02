e2505e249cc47821b1b937176b4924779ac11577
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-02-02 10:14:49 +0100

    Mock data done

--- TeacherClient/src/data/Data.java
+++ TeacherClient/src/data/Data.java
@@ -5,10 +5,48 @@
  */
 package data;
 
+import be.Student;
+import be.Class;
+import java.util.List;
+
 /**
  *
  * @author alexl
  */
 public class Data {
+    
+    private List<Class> listOne;
+    private List<Class> listTwo;
+    private List<Class> listThree;
+    
+    private Class SCO;
+    private Class SDE;
+    private Class ITO;
+    private Class Sløjd;
+    
+    private Student Alex;
+    private Student Asbjørn;
+    private Student Jan;
+    
+    public void createMockData() {
+        SCO = new Class(1, "SCO");
+        SDE = new Class(2, "SDE");
+        ITO = new Class(3, "ITO");
+        Sløjd = new Class(4, "Sløjd");
+        
+        listOne.add(SCO);
+        listOne.add(ITO);
+        
+        listTwo.add(SCO);
+        listTwo.add(SDE);
+        listTwo.add(ITO);
+        
+        listThree.add(SDE);
+        listThree.add(Sløjd);
+        
+        Alex = new Student(1, "Alex Tygesen", listOne);
+        Asbjørn = new Student(2, "Asbjørn Mansa EtEllerAndet", listTwo);
+        Jan = new Student(3, "JanvanZetten", listThree);
+    }
     
 }
--- TeacherClient/src/gui/model/MainModel.java
+++ TeacherClient/src/gui/model/MainModel.java
@@ -7,14 +7,20 @@ package gui.model;
 
 import be.Student;
 import be.Class;
+import data.Data;
 
 /**
  *
  * @author alexl
  */
 public class MainModel {
+    
+    private Data data;
 
     public void createMockData() {
+        data = new Data();
+        data.createMockData();
+        
         new Class(1, "SCO");
         new Class(2, "SDE");
         new Class(3, "ITO");
