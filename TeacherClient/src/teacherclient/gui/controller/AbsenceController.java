4f2ccb6e1d5da9adc905d8cb3da8999dfe689cd6
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-02-09 12:25:42 +0100

    bug fix for student list

--- TeacherClient/src/teacherclient/gui/controller/AbsenceController.java
+++ TeacherClient/src/teacherclient/gui/controller/AbsenceController.java
@@ -33,6 +33,7 @@ public class AbsenceController implements Initializable {
     private Label labelClass;
     
     private AbsenceModel model;
+    private Student selectedStudent;
 
     /**
      * Initializes the controller class.
@@ -50,7 +51,8 @@ public class AbsenceController implements Initializable {
 
     @FXML
     private void studentSelected(MouseEvent event) {
-        if (listviewStudents.getSelectionModel().getSelectedItem() != null) {
+        if (listviewStudents.getSelectionModel().getSelectedItem() != null && selectedStudent != listviewStudents.getSelectionModel().getSelectedItem()) {
+            selectedStudent = listviewStudents.getSelectionModel().getSelectedItem();
             System.out.println(listviewStudents.getSelectionModel().getSelectedItem());
             
         }
