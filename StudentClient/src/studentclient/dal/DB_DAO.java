0afa58ff1040f526d1f8a1afc605ad9dc70a8179
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-03-16 12:56:45 +0100

    login for database

--- StudentClient/src/studentclient/dal/DB_DAO.java
+++ StudentClient/src/studentclient/dal/DB_DAO.java
@@ -2,11 +2,11 @@
 package studentclient.dal;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import studentclient.be.Student;
 
 /**
  *
@@ -32,6 +32,47 @@ public class DB_DAO
             ResultSet rs = st.executeQuery(sql);
 
             
+
+        }
+        catch (SQLException ex)
+        {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        }
+    }
+    
+    
+    /**
+     * Get student the student with this login
+     * @param username
+     * @param encryptedPassword
+     * @return
+     * @throws DALException if there was an error or when the username password combo does not exist in database
+     */
+    public Student login(String username, String encryptedPassword) throws DALException{
+        try (Connection con = connecter.getConnection())
+        {
+            
+            String sql = "SELECT * FROM Student WHERE username = ? AND password = ?;";
+            
+            PreparedStatement statement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            statement.setString(1, username);
+            statement.setString(2, encryptedPassword);
+            
+            statement.execute();
+            ResultSet rs = statement.getGeneratedKeys();
+            
+            if (rs.next()){
+                int id = rs.getInt("id");
+                String fName = rs.getString("fName");
+                String lName = rs.getString("lName");
+                
+                Student student = new Student(id, fName + lName, username, encryptedPassword);
+                return student;
+            }
+            else{
+                throw new DALException("Check your Username and password");
+            }
 
         }
         catch (SQLException ex)
